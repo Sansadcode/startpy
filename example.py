4a89eb04234b3430d904d0607b18ec03faa4eb9b
Sansadcode <sanobiasadaf@gmail.com>
2025-02-22 12:54:29 -0700

    updated files

--- example.py
+++ example.py
@@ -1,13 +1,16 @@
-# print("this is {} eg.".format(var1, var2))
-# var1 = "print"
-# var2 = "print2"
-
-#adding the user in linux 
-def new_user():
-    confirm = "N"
-    while confirm != "Y":
-        username = input ("enter the name of user to add ")
-        print("use the username '" + username + "'? (Y/N)")
-        confirm = input().upper()
-        #will call the below command in linux
-        os.system("sudo adduser" + username)
+# # print("this is {} eg.".format(var1, var2))
+# # var1 = "print"
+# # var2 = "print2"
+#
+# #adding the user in linux
+# def new_user():
+#     confirm = "N"
+#     while confirm != "Y":
+#         username = input ("enter the name of user to add ")
+#         print("use the username '" + username + "'? (Y/N)")
+#         confirm = input().upper()
+#         #will call the below command in linux
+#         os.system("sudo adduser" + username)
+print(3**2)
+msg = "the answer is"
+**msg2 = msg**
---
+++ example1.py
@@ -0,0 +1,62 @@
+# # # lst = [[x for x in range (3)] for y in range(3)]
+# # #
+# # # for r in range(3):
+# # #     for c in range(3):
+# # #         if lst[r][c] % 2 != 0:
+# # #             print("#")
+# # def function_1(a):
+# #     return None
+# #
+# #
+# # # def function_2(a):
+# # #     return function_1(a) * function_1(a)
+# # # #
+# # # # print(function_2(2))
+# # # print(1 // 2)
+# #
+# # # def func(a, b):
+# # #     return b ** a
+# # #
+# # # # print(func(2, 2))
+# # # my_list =  [x * x for x in range(5)]
+# # #
+# # #
+# # # def fun(lst):
+# # #     del lst[lst[2]]
+# # #     return lst
+# # #
+# # #
+# # # print(fun(my_list))
+# # #
+# # a = 1
+# # b = 0
+# # a = a ^ b
+# # b = a ^ b
+# # a = a ^ b
+# #
+# # print(a, b)
+# def fun(x):
+#     if x % 2 == 0:
+#         return 1
+#     else:
+#         return 2
+#
+#
+# print(fun(fun(5)))
+#
+# def fun(in = 2, out = 3)
+#     return in * out
+#     print(fun(3))
+
+try:
+    first_prompt = input("enter the first vlaue")
+    a = len(first_prompt)
+    second_prompt = input("enter the second value")
+    b = len(second_prompt) * 2
+    print(a//b)
+except ZeroDivisionError:
+    print("Do not divide by zero")
+except ValueError:
+    print("wrong value")
+except:
+    print("error.error")
